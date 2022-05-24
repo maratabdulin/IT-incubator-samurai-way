0e2df051f15a3cc5dc8b3bc523fde9c6699dbad4
Marat <marato13@ya.ru>
2022-05-24 13:32:23 +0300

    add 1-10 lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/accordion/accordion";
-import Rating from "./components/rating/rating";
+import Header from "./components/header/Header";
+import Technology from "./components/technology/Technology";
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
-        <div className="App">
-            <AppTitle title={'This is APP component'}/>
-            Article 1
-            <Accordion titleValue={'Menu'} collapsed={false}/>
-            <Accordion titleValue={'User list'} collapsed={false}/>
-            Article 2
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+        <div>
+            <Header/>
+            <Technology/>
+            <Footer/>
         </div>
     );
-}
-
-type AppTitlePropsType = {
-    title: string
-}
-
-const AppTitle = (props: AppTitlePropsType) => {
-    return (
-        <h1>
-            {props.title}
-        </h1>
-    )
 }
 
 export default App;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News feed</a>
+            <a href="#">Messages</a>
+        </div>
+    )
+}
+
+export default Footer;
