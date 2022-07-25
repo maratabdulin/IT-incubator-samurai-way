18bd91ff87ff310708277f3399941de92b0b9c3d
Marat <marato13@ya.ru>
2022-07-25 13:51:47 +0300

    add 54 lesson. add componentDidMount

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -5,9 +5,7 @@ import axios from "axios";
 import userDefaultPhoto from '../../assets/images/user-image.jpg'
 
 class Users extends React.Component<UsersPropsType> {
-
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios
             .get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
